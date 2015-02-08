d76771d50cd04cc965e555b908266c91f47b77c0
Qays Poonawala <qayshp@gmail.com>
2015-02-07 19:31:02 -0800

    Pull live data from API for Fair Market Rents and Multi Family Properties.

--- homeful/models.py
+++ homeful/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+import requests
+import json
 
-# Create your models here.
+SEATTLE_FAIR_MARKET_RENTS_URL = 'http://services.arcgis.com/VTyQ9soqVukalItT/arcgis/rest/services/FairMarketRents/FeatureServer/0/query?where=&objectIds=&time=&geometry=%7B%22xmin%22%3A-13721209.831341939%2C%22ymin%22%3A6018679.182711435%2C%22xmax%22%3A-13514676.980915314%2C%22ymax%22%3A6064541.399682558%2C%22spatialReference%22%3A%7B%22wkid%22%3A102100%7D%7D&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Meter&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&quantizationParameters=&f=pjson&token='
+SEATTLE_MULTI_FAMILY_PROPERTIES = 'http://services.arcgis.com/VTyQ9soqVukalItT/arcgis/rest/services/MultiFamilyProperties/FeatureServer/0/query?where=&objectIds=&time=&geometry=%7B%22xmin%22%3A-13721209.831341939%2C%22ymin%22%3A6018679.182711435%2C%22xmax%22%3A-13514676.980915314%2C%22ymax%22%3A6064541.399682558%2C%22spatialReference%22%3A%7B%22wkid%22%3A102100%7D%7D&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Meter&outFields=*&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&quantizationParameters=&f=pjson&token='
+
+def populateSeattleFairMarketRents():
+    list_of_fair_market_rents = requests.get(SEATTLE_FAIR_MARKET_RENTS_URL).json().get(u'features')
+    for fair_market_rents in list_of_fair_market_rents:
+        fair_market_rent_data = fair_market_rents.get(u'attributes')
+        
+        fair_market_rent_data.get(u'FMR_0BDR')
+        fair_market_rent_data.get(u'FMR_1BDR')
+        fair_market_rent_data.get(u'FMR_2BDR')
+        fair_market_rent_data.get(u'FMR_3BDR')
+        fair_market_rent_data.get(u'FMR_4BDR')
+        fair_market_rent_data.get(u'FMR_AREANAME')
+
+def populateSeattleMultiFamilyProperties():
+    list_of_multi_family_properties = requests.get(SEATTLE_MULTI_FAMILY_PROPERTIES).json().get(u'features')
+    for multi_family_property in list_of_multi_family_properties:
+        multi_family_property_data = multi_family_property.get(u'attributes')
+        
+        std_addr = multi_family_property_data.get('STD_ADDR')
+        multi_family_property_data.get('STD_CITY')
+        multi_family_property_data.get('STD_ST')
+        multi_family_property_data.get('STD_ZIP5')
+        multi_family_property_data.get('LAT')
+        multi_family_property_data.get('LON')
+        multi_family_property_data.get('PROPERTY_NAME_TEXT')
+        multi_family_property_data.get('PROPERTY_ON_SITE_PHONE_NUMBER')
+        print std_addr
--- homeful/views.py
+++ homeful/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+import logging
+import utils
+
+logger = logging.getLogger(__name__)
 
 def index(request):
     context = {}
     return render(request, 'homeful/index.html', context)
+
+def test(request):
+    context = {}
+    return render(request, 'homeful/test.html', context)
